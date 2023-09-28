7ca81516472dde4c0186ed4492c3b77ca9ab3c77
AbdullahLaham <abed26194@gmail.com>
2023-09-28 15:02:34 +0300

    ch

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -23,7 +23,7 @@ const EmojiPicker = ({ onChange }: EmojiPickerProps) => {
             <Smile className='text-neutral-500 hover:text-neutral-600 dark:text-neutral-300 transition' />
         </PopoverTrigger>
         <PopoverContent side='right' sideOffset={40} className='bg-transparent border-none shadow-none drop-shadow-none mb-16 ' >
-            <Picker theme={resolvedTheme} data={data} onEmojiSelect={(emoji: any) => onChange(emoji.native)} />
+            {/* <Picker theme={resolvedTheme} data={data} onEmojiSelect={(emoji: any) => onChange(emoji.native)} /> */}
         </PopoverContent>
     </Popover>
 
